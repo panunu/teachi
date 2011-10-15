6f1a0130646a988bbccff624cfd2b7d1330e1bea
Panu Leppäniemi <me@panuleppaniemi.com>
2011-10-16 02:07:13 +0300

    Added ChapterRepository and -Service.

---
+++ src/Teachi/MaterialBundle/Repository/ChapterRepository.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Teachi\MaterialBundle\Repository;
+
+use Doctrine\ORM\EntityRepository as Repository,
+    Doctrine\ORM\Query\Expr\Join as Join;
+
+class ChapterRepository extends Repository
+{
+    public function findChapterWithContentsByNumberAndBookLink($number, $book)
+    {
+        $qb = $this->_em->createQueryBuilder()->select('chapter')
+            ->from('Teachi\MaterialBundle\Entity\Chapter', 'chapter')
+            ->join('chapter.book', 'book', Join::WITH, 'book.link = :link')
+            ->where('chapter.number = :number')
+            ->setParameter('number', $number)
+            ->setParameter('link',   $book);
+        
+        return $qb->getQuery()->getSingleResult();
+    }
+}
---
+++ src/Teachi/MaterialBundle/Service/ChapterService.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Teachi\MaterialBundle\Service;
+
+use Doctrine\ORM\EntityManager,
+    Doctrine\ORM\EntityRepository;
+
+class ChapterService
+{
+    /**
+     * @var EntityManager
+     */
+    private $em;
+
+    /**
+     * @var BookRepository
+     */
+    private $repository;
+
+    public function __construct(EntityManager $em, EntityRepository $repository)
+    {
+        $this->em         = $em;
+        $this->repository = $repository;
+    }
+
+    /**
+     * @param  int    $number 
+     * @param  string $link 
+     * @return Book
+     */
+    public function getChapterWithContentsByNumberAndBookLink($number, $link)
+    {
+        return $this->repository->findChapterWithContentsByNumberAndBookLink(
+            $number, $link
+        );
+    }
+}
