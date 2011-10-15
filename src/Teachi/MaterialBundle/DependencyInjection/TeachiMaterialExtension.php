bdbff5434f5cf45a39e85ab189ea0e4e21852ae1
Panu Leppäniemi <me@panuleppaniemi.com>
2011-10-16 02:09:17 +0300

    Clean up.

--- src/Teachi/MaterialBundle/DependencyInjection/TeachiMaterialExtension.php
+++ src/Teachi/MaterialBundle/DependencyInjection/TeachiMaterialExtension.php
@@ -12,7 +12,7 @@ class TeachiMaterialExtension extends Extension
     public function load(array $config, ContainerBuilder $container)
     {      
         $loader = new Loader\YamlFileLoader(
-            $container, new FileLocator(__DIR__.'/../Resources/config')
+            $container, new FileLocator(__DIR__ . '/../Resources/config')
         );
         
         $loader->load('services.yml');
